b929aad6bc5d364bff7d42324416b30a03392de8
Fábio Monari <fabiomonari@gmail.com>
2022-04-28 09:36:48 -0300

    Adicionando menu toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MoviesListComponent } from './movies-list/movies-list.component';
 
@@ -26,6 +28,8 @@ import { MoviesListComponent } from './movies-list/movies-list.component';
     MatPaginatorModule,
     ReactiveFormsModule,
     MatProgressBarModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
